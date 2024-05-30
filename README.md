5621c2a8db69d90fb769b05bf4b2ec32b6656793
Fabio Calderon <FabiooCalderon@gmail.com>
2024-05-29 22:38:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Front End para aplicación de nómina
+
+Para correr la aplicación em modo de desarrollador correr el siguiente comando: npm run dev
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <p>Placeholder Frontend App nomina</p>
+    </>
+  );
+}
+
+export default App;
