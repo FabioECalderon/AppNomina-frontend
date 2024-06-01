aaabc6ec6fe88bd00b39c140f9d64a93ef4f29cc
Fabio Calderon <FabiooCalderon@gmail.com>
2024-05-31 22:53:31 -0500

    Tailwind integration

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Front End para aplicación de nómina
 
+Después de descargar el repositorio se debe instalar las dependencias con el comando: npm i
 Para correr la aplicación em modo de desarrollador correr el siguiente comando: npm run dev
--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <>
-      <p>Placeholder Frontend App nomina</p>
+      <p className="text-3xl font-bold underline">
+        Placeholder Frontend App nomina with Tailwind
+      </p>
     </>
   );
 }
