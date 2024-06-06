7715c26d196d1ecc4ef63cbd0800c7eb600b9565
Fabio Calderon <FabiooCalderon@gmail.com>
2024-06-05 23:55:46 -0500

    Login and register pages initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import { Button } from './components/ui/button';
+
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -8,6 +12,10 @@ function App() {
         Placeholder Frontend App nomina with Tailwind
       </p>
       <Button>Click me</Button>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </>
   );
 }
