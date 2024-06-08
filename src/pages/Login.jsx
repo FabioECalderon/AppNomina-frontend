4956f5689c9f33598a40200760c4c9d60086d01b
Fabio Calderon <FabiooCalderon@gmail.com>
2024-06-07 23:57:32 -0500

    Added form for login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { z } from 'zod';
+
+const formSchema = z.object({
+  username: z.string().min(6).max(50),
+  password: z.string().min(8),
+});
 
 export default function Login() {
-  return <div>Login</div>;
+  return <></>;
 }
