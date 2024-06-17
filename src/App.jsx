96c9d82eea15228a236c993f74821089fc1dd850
Fabio Calderon <FabiooCalderon@gmail.com>
2024-06-16 23:28:17 -0500

    Refactor routes using App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,24 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { Button } from './components/ui/button';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import EmployeesHome from './pages/EmployeesHome.jsx';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import NotFoundPage from './pages/NotFoundPage';
+import EmployeeDetail from './pages/EmployeeDetail';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <EmployeesHome />,
+    errorElement: <NotFoundPage />,
+  },
+  { path: '/login', element: <Login /> },
+  { path: '/register', element: <Register /> },
+  { path: '/empleado/:employeeId', element: <EmployeeDetail /> },
+]);
 
 function App() {
   return (
@@ -12,10 +28,7 @@ function App() {
         Placeholder Frontend App nomina with Tailwind
       </p>
       <Button>Click me</Button>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <RouterProvider router={router} />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App.jsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
-import EmployeesHome from './pages/EmployeesHome.jsx';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import NotFoundPage from './pages/NotFoundPage';
-import EmployeeDetail from './pages/EmployeeDetail';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <EmployeesHome />,
-    errorElement: <NotFoundPage />,
-  },
-  { path: '/login', element: <Login /> },
-  { path: '/register', element: <Register /> },
-  { path: '/empleado/:employeeId', element: <EmployeeDetail /> },
-]);
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 );
