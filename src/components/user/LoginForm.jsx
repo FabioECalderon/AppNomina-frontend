807e495378a1cd45ee70d81e879bf2e875e4dab2
Fabio Calderon <FabiooCalderon@gmail.com>
2024-06-18 23:33:01 -0500

    Login form initial commit

--- src/components/user/LoginForm.jsx
+++ src/components/user/LoginForm.jsx
@@ -7,6 +7,8 @@ import {
   FormLabel,
   FormMessage,
 } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
@@ -25,20 +27,27 @@ const formSchema = z.object({
 });
 
 export default function LoginForm() {
-  // // 1. Define your form.
-  // const form =
-  //   useForm <
-  //   z.infer <
-  //   typeof formSchema >>
-  //     {
-  //       resolver: zodResolver(formSchema),
-  //       defaultValues: {
-  //         username: '',
-  //       },
-  //     };
-  //     function onSubmit(values: z.infer<typeof formSchema>) {
-  //       // Do something with the form values.
-  //       // ✅ This will be type-safe and validated.
-  //       console.log(values)
-  //     }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 }
