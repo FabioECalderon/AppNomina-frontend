c97e35e0dd3858e0b6e649d12f6c0380d0606dee
Fabio Calderon <FabiooCalderon@gmail.com>
2024-08-10 23:39:33 -0500

    Fix background margins and register validations

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,7 @@ import RegisterForm from '@/components/user/RegisterForm';
 export default function Register() {
   return (
     <>
-      <main className="bg-blue-400">
+      <main className="bg-skin-fill m-0 p-0 w-[100%]">
         <RegisterForm />
       </main>
     </>
